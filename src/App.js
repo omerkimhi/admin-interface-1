4d09a0d9788e82107d196e42b9c0e8c54e6c4a29
Omer Kimhi <omerk2910@gmail.com>
2020-04-10 18:20:54 +0300

    10/4 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./Components/login";
 
 function App() {
   return (
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top"/>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+          </Switch>
+        </div>
+      </div>
     </div>
+  </Router>
   );
 }
 
---
+++ src/Components/login.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            Email: "",
+            Password: "",
+            
+        };
+    }
+add=()=>{
+
+    
+    fetch('http://reqres.in/api/users')
+    .then(res=>console.log(res))
+      
+    //getUserData = (data) => {
+     //   this.setState({
+      //      userName: data
+     //   }, () => { console.log(this.state.userName) })
+
+  //  }
+
+  //  componentDidMount() {
+   //     this.apiUrl = "http://proj.ruppin.ac.il/igroup17/Mobile/project/api/spazioUser/";
+
+  //  }
+
+
+
+
+}
+
+    render() {
+        return (
+            <form>
+                <h2>Admin Interface</h2>
+                <h3>Sign In</h3>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block" >Login</button>
+                
+            </form>
+        );
+    }
+}
