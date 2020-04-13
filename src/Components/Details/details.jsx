820cd3179770893edc25c99d1541261aeaa4f3bc
Omer Kimhi <omerk2910@gmail.com>
2020-04-13 17:23:30 +0300

    13/4 17:20

--- src/Components/Details/details.jsx
+++ src/Components/Details/details.jsx
@@ -11,6 +11,10 @@ import { savePDF } from '@progress/kendo-react-pdf';
 import { Ripple } from '@progress/kendo-react-ripple';
 import '@progress/kendo-theme-material/dist/all.css'; 
 
+import { DonutChartContainer } from '../Charts/DonutChartContainer';
+import { BarChartContainer } from '../Charts/BarChartContainer';
+import { GridContainer } from '../Grid/GridContainer';
+
 //import { Card, Container,Row,Col } from 'react-bootstrap';
 //import NavBar from '../NavBar/NavBar.jsx'
 
@@ -36,18 +40,21 @@ import '@progress/kendo-theme-material/dist/all.css';
 
   render() {
     return (
+     
       <Ripple>
+         <br/>
+      <br/>
       <div className="bootstrap-wrapper">
         <div className="app-container container" ref={(el) => this.appContainer = el}>
           <div className="row">
             <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-              <h1>Sales | Q4 2018</h1>
-            </div>
-            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 buttons-right">
-  <Button primary={true} onClick={this.handleShare}>Share</Button>
-  <Button onClick={this.handlePDFExport}>Export to PDF</Button>
-</div>
-          </div>
+              <h1>My Stats</h1>
+                 </div>
+                 <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 buttons-right">
+                 <Button primary={true} onClick={this.handleShare}>Share</Button>
+                 <Button onClick={this.handlePDFExport}>Export to PDF</Button>
+                 </div>
+                 </div>
           <div className="row">
             <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
               <h4>Panel Bar Container</h4>
@@ -55,27 +62,39 @@ import '@progress/kendo-theme-material/dist/all.css';
             <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9 col-xl-9">
               <div className="row">
                 <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4 col-xl-4">
-                  <h4>Donut Chart Container</h4>
+               
                 </div>
                 <div className="col-xs-6 col-sm-6 col-md-2 col-lg-2 col-xl-2">
                   <div className="percentage-container">
                     <span className="percentage-number">94</span>
                     <span className="percentage-sign">%</span>
-                    <p>CUSTOMER SATISFACTION</p>
+                    <p>Percentage of income compared to last month</p>
                   </div>
                   <div className="percentage-container">
                     <span className="percentage-number">89</span>
                     <span className="percentage-sign">%</span>
-                    <p>TARGET SALES</p>
+                    <p>Percentage of income compared to last year</p>
                   </div>
                 </div>
                 <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                  <h4>Bar Chart Container</h4>
+                  <div>
+                    <h3>My Rank</h3>
+                    <span/>
+                    <h4>Average Rank in my field</h4>
+                    <span/>
+                    <h3>My Equipment Rank</h3>
+                    <span/>
+                    <h4>Average  Equipment Rank in my field</h4>
+                    <span/>
+                    
+
+                  </div>
                 </div>
               </div>
               <div className="row">
                 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                  <h4>Grid Container</h4>
+                hhhhh
+
                 </div>
               </div>
             </div>
